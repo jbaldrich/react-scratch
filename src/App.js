7fb3cd8a815c3dbb44491ceb1ea8c944ae66ec30
Jaco Baldrich <jb@jacobaldrich.com>
2019-01-17 13:11:27 +0100

    install react-hot-loader and set up a script for building

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {hot} from 'react-hot-loader';
 import './App.css';
 
 const App = props => {
@@ -7,4 +8,4 @@ const App = props => {
     );
 };
 
-export default App;
+export default hot(module)(App);
